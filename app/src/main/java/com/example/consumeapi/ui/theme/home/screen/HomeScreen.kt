70ec1188f73642a6b348cdd1ed92cf2842a0b11d
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2023-12-28 19:36:31 +0700

    Membuat Function HomeScreen

--- app/src/main/java/com/example/consumeapi/ui/theme/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/theme/home/screen/HomeScreen.kt
@@ -1,68 +1,53 @@
 package com.example.consumeapi.ui.theme.home.screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
-import com.example.consumeapi.model.Kontak
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.consumeapi.navigation.DestinasiNavigasi
+import com.example.consumeapi.ui.theme.PenyediaViewModel
+import com.example.consumeapi.ui.theme.home.viewmodel.HomeViewModel
 
-@Composable
-fun KontakCard(
-    kontak: Kontak
-    onDeleteClick: (Kontak) -> Unit = {},
-    modifier: Modifier = Modifier
-){
-    Row (
-        modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = kontak.email,
-            style = MaterialTheme.typography.titleMedium
-        )
-        Spacer(Modifier.weight(1f))
-        IconButton(onClick = { onDeleteClick(kontak) }) {
-            Icon(
-                imageVector = Icons.Default.Delete,
-                contentDescription = null,
-            )
-        }
-    }
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Kontak"
 }
 @Composable
-fun KontakLayout(
-    kontak: List<Kontak>,
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
     modifier: Modifier = Modifier,
-    onDetailClick: (Kontak) -> Unit,
-    onDeleteClick: (Kontak) -> Unit = {}
-) {
-    LazyColumn(
-        modifier = modifier,
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        items(kontak) { kontak ->
-            KontakCard(
-                kontak = kontak,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable { onDetailClick(kontak) },
-                onDeleteClick = {
-                    onDeleteClick(kontak)
-                }
+    onDetailClick: (Int) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val  scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarKontak(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
             )
-        }
-    }
-}
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Kontak"
+                )
+            }
+        },
+    ){
