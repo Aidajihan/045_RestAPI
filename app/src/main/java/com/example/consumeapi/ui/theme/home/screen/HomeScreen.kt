c3cbc58b99af5203ac720e71dd9a893104d90075
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2023-12-28 19:23:37 +0700

    Membuat file HomeScreen dan menambahkan function KontakLayout

--- app/src/main/java/com/example/consumeapi/ui/theme/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/theme/home/screen/HomeScreen.kt
@@ -1,8 +1,11 @@
 package com.example.consumeapi.ui.theme.home.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Icon
@@ -12,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.consumeapi.model.Kontak
 
 @Composable
@@ -33,6 +37,31 @@ fun KontakCard(
             Icon(
                 imageVector = Icons.Default.Delete,
                 contentDescription = null,
+            )
+        }
+    }
+}
+@Composable
+fun KontakLayout(
+    kontak: List<Kontak>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Kontak) -> Unit,
+    onDeleteClick: (Kontak) -> Unit = {}
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(kontak) { kontak ->
+            KontakCard(
+                kontak = kontak,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(kontak) },
+                onDeleteClick = {
+                    onDeleteClick(kontak)
+                }
             )
         }
     }
